dfb55f3e9e3c9130ab7e36ece334a5428c677937
Rohini Carlson <it-hogskolan@it-hogskolans-MacBook-Pro-96.local>
2015-01-23 00:20:08 +0100

    Updated colors, fonts and added button functionality

--- Labb1/ViewController.m
+++ Labb1/ViewController.m
@@ -9,14 +9,33 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *cakeImage;
+@property (weak, nonatomic) IBOutlet UIButton *nextButton;
+@property (weak, nonatomic) IBOutlet UIButton *previousButton;
+
 
 @end
 
 @implementation ViewController
 
+- (IBAction)showNextImage:(id)sender {
+    self.cakeImage.image = [UIImage imageNamed:@"me.jpg"];
+    self.previousButton.enabled = YES;
+    self.nextButton.enabled = NO;
+}
+
+
+- (IBAction)showPreviousImage:(id)sender {
+    self.cakeImage.image = [UIImage imageNamed:@"ariel.jpg"];
+    self.previousButton.enabled = NO;
+    self.nextButton.enabled = YES;
+}
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.previousButton.enabled = NO;
 }
 
 - (void)didReceiveMemoryWarning {
